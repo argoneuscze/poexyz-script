464472f4120841d27425344f8a493af01126e109
argoneus <argoneuscze@gmail.com>
2014-09-06 13:48:01 +0200

    initial

---
+++ poexyz_script.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name       poexyz Script
+// @namespace  http://www.argoneus.com
+// @version    0.1
+// @description  A script to improve the user experience at poexyz
+// @match      *://poe.xyz.is/search/*
+// @copyright  2014+, argoneus
+// ==/UserScript==
+
