06b36fc7f687c55af2d172b922d37cd7f715896b
argoneus <argoneuscze@gmail.com>
2014-09-06 14:23:17 +0200

    added jquery

--- poexyz_script.user.js
+++ poexyz_script.user.js
@@ -5,5 +5,7 @@
 // @description  A script to improve the user experience at poexyz
 // @match      *://poe.xyz.is/search/*
 // @copyright  2014+, argoneus
+// @require http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 // ==/UserScript==
 
+this.$ = this.jQuery = jQuery.noConflict(true);
